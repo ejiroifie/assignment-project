2ac093a72f95fb19d31c8d1676ac1d7a661b02fa
ejiroifie <ejiroifie@gmail.com>
2019-07-08 22:05:17 -0400

    switching js code to js file

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 
-        function mult() {
-           num1 = document.getElementById ("firstNumber").value;
-           num2 = document.getElementById ("secondNumber").value;
-           document.getElementById ("result").innerHTML = num1* num2;
-        }
-    
+   function mult() {
+         num1 = document.getElementById ("firstNumber").value;
+         num2 = document.getElementById ("secondNumber").value;
+         document.getElementById ("result").innerHTML = num1* num2;
+        
+      }
+    function div ()  {
+      num1 = document.getElementById ("firstNumber").value;
+      num2 = document.getElementById ("secondNumber").value;
+      document.getElementById ("result").innerHTML = (num1 / num2);
+
+    }
+
+    function add ()  {
+      num1 = document.getElementById ("firstNumber").value;
+      num2 = document.getElementById ("secondNumber").value;
+      document.getElementById ("result").innerHTML = (num1 + num2);
+
+    }
+
+    function sub ()  {
+      num1 = parseInt(document.getElementById ("firstNumber").value);
+      num2 = parseInt(document.getElementById ("secondNumber").value);
+      document.getElementById ("result").innerHTML = (num1 - num2);
+   }
+  
